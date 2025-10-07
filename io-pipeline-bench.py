65f8d8734cccd7d09f2d5b27978662360cdd6f37
Phil Carns <carns@mcs.anl.gov>
2025-10-07 15:31:32 -0400

    stub pipeline steps

--- io-pipeline-bench.py
+++ io-pipeline-bench.py
@@ -6,11 +6,13 @@ import time
 
 # base class for pipeline
 class pipeline:
-    def __init__(self, buffer_size_bytes: int, concurrency: int):
+    def __init__(self, buffer_size_bytes: int, concurrency: int, recv_delay:
+                 float):
         self.buffer_size_bytes = buffer_size_bytes
         self.concurrency = concurrency
         self.buffers_xferred = 0
         self.elapsed = 0
+        self.recv_delay = recv_delay
 
         self._buffer = array.array('B', bytes(self.buffer_size_bytes))
 
@@ -25,21 +27,43 @@ class pipeline:
 
 # sequential version of pipeline
 class pipeline_sequential(pipeline):
-    def __init__(self, buffer_size_bytes: int, concurrency: int):
+    def __init__(self, buffer_size_bytes: int, concurrency: int, recv_delay:
+                 float):
 
         if concurrency != 1:
             raise ValueError(f"Invalid 'concurrency' value for pipeline_sequential. Expected 1, but got {concurrency}.")
 
         super().__init__(buffer_size_bytes=buffer_size_bytes,
-                         concurrency=concurrency)
+                         concurrency=concurrency, recv_delay=recv_delay)
+
+    def _recv(self, buffer: array.array):
+        # don't do anything except wait for configurable time
+        time.sleep(self.recv_delay)
+
+    def _compute(self, buffer: array.array):
+        pass
+
+    def _write(self, buffer: array.array):
+        pass
 
     def run(self, duration_s: int):
 
         start_ts = time.perf_counter()
 
         while (time.perf_counter() - start_ts) < duration_s:
+            # 3 steps per pipeline:
+
+            # recv data (fake)
+            self._recv(self._buffer)
+
+            # compute (rnd generator)
+            self._compute(self._buffer)
+
+            # write (write and flush)
+            self._write(self._buffer)
+
+            # bookkeeping
             self.buffers_xferred += 1
-            time.sleep(.1)
 
         self.elapsed = time.perf_counter() - start_ts
 
@@ -53,12 +77,15 @@ def main():
                         help='Size of each transfer buffer in KiB.')
     parser.add_argument('--concurrency', type=int, required=True,
                         help='Level of concurrency.')
+    parser.add_argument('--recv-delay', type=float, required=True,
+                        help='(float) seconds to sleep per emulated recv')
     args = parser.parse_args()
 
     if args.method == "sequential":
         my_pipeline = pipeline_sequential(buffer_size_bytes=
                                           (args.buffer_size*1024),
-                                          concurrency=args.concurrency);
+                                          concurrency=args.concurrency,
+                                          recv_delay=args.recv_delay);
     else:
         raise ValueError(f"Invalid method: {args.method}")
 
