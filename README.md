cfdba8dd38d4986b8c1a377897e18f27e9929ae8
Phil Carns <carns@mcs.anl.gov>
2025-10-07 14:18:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# io-pipeline-bench
+
+This is a simple benchmark to measure the throughput of various I/O
+pipelining methods in Python.
+
+## Installation
+
+* create a python venv
+* `pip install -r requirements.txt`
---
+++ io-pipeline-bench.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--method', type=str, required=True,
+                        help='What pipeline method to use.')
+    parser.add_argument('--duration', type=int, required=True,
+                        help='How many seconds to execute.')
+    parser.add_argument('--buffer-size', type=int, required=True,
+                        help='Size of each transfer buffer in KiB.')
+    parser.add_argument('--concurrency', type=int, required=True,
+                        help='Level of concurrency.')
+    args = parser.parse_args()
+
+    # Add your logic here
+    pass
+
+if __name__ == "__main__":
+    main()
